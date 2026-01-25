820b5e00864f9f4b6792af72875e24cc41b92361
altencirsilvajr <altencirsilvajr@gmail.com>
2026-01-24 21:49:18 -0300

    feat: route in toLowerCase

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
